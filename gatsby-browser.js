cfd64e4f2d12dc87decc67b462e9440bb62767a9
Jimmy Timmons <timmonsjg@gmail.com>
2018-02-27 09:10:36 -0500

    Re-add GA in a gatsby-esque manner

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- // You can delete this file if you're not using it
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-91648771-3');
+
+exports.onRouteUpdate = (state, page, pages) => {
+	ReactGA.pageview(state.location.pathname);
+};
+
+
