6845e4b0d589c1b0d5571242a579371ec2bdea5c
Jimmy Timmons <timmonsjg@gmail.com>
2018-08-21 21:24:53 -0400

    API key

--- src/pages/hotel.js
+++ src/pages/hotel.js
@@ -64,7 +64,7 @@ class Hotel extends Component {
                     <MapWrapper 
                         markers={markers}
                         zoom={13}
-                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtW91lsq39NAQvLfi9rUCux8O4Rb6YYXM&v=3.exp&libraries=geometry,drawing,places"
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyA9NbyQdgdMCPwv11_EOwwmVaDC566mKTw&v=3.exp&libraries=geometry,drawing,places"
                         loadingElement={<div style={{ height: `100%` }} />}
                         containerElement={<div style={{ height: `400px` }} />}
                         mapElement={<div style={{ height: `100%` }} />}
